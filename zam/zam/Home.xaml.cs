66e7fb1601a0920ecc9173092d9db43abbc3b946
user <user@DARREN>
2024-05-07 12:41:32 +0100

    Add project files.

---
+++ zam/zam/Home.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zam
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Home : ContentPage
+	{
+		public Home()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+
+        }
+
+		private void Button_Clicked_1(object sender, EventArgs e)
+		{
+
+        }
+
+		private void Button_Clicked_2(object sender, EventArgs e)
+		{
+
+        }
+
+		private void Button_Clicked_3(object sender, EventArgs e)
+		{
+
+        }
+
+		private void Button_Clicked_4(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_5(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_6(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_7(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_8(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_9(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_10(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button_Clicked_11(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
